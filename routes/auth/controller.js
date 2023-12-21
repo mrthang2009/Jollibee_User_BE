cbb68a85168b584e834187b6ca6ac616a6a069fd
Mr.Thang <vothang226@gmail.com>
2023-12-21 22:20:27 +0700

    up

--- routes/auth/controller.js
+++ routes/auth/controller.js
@@ -11,73 +11,6 @@ const { Customer, Cart } = require("../../models");
 let storedVerificationCode; // Đặt biến ngoài phạm vi hàm
 
 module.exports = {
-  login: async (req, res, next) => {
-    try {
-      const { _id, firstName, lastName, email } = req.user;
-
-      const token = generateToken({
-        _id,
-        firstName,
-        lastName,
-        email,
-      });
-
-      const refreshToken = generateRefreshToken(_id);
-
-      return res.status(200).json({
-        message: "Login of user successfully",
-        token,
-        refreshToken,
-      });
-    } catch (err) {
-      return res
-        .status(500)
-        .json({ message: "Login of user failed", error: err });
-    }
-  },
-  sendCode: async (req, res, next) => {
-    try {
-      const { email, phoneNumber, forgotPassword } = req.body;
-
-      const getEmailExits = Customer.findOne({ email });
-      const getPhoneExits = Customer.findOne({ phoneNumber });
-
-      const [foundEmail, foundPhoneNumber] = await Promise.all([
-        getEmailExits,
-        getPhoneExits,
-      ]);
-
-      const errors = [];
-      if (!forgotPassword) {
-        if (foundEmail) errors.push("Email đã tồn tại");
-        if (foundPhoneNumber) errors.push("Số điện thoại đã tồn tại");
-      } else {
-        if (!foundEmail) errors.push("Email tài khoản không tồn tại");
-      }
-
-      if (errors.length > 0) {
-        return res.status(404).json({
-          message: "Gửi mã xác nhận thất bại",
-          error: errors.join(", "),
-        });
-      }
-
-      // Tạo và gửi mã xác nhận
-      const verificationCode = generateVerificationCode();
-      storedVerificationCode = verificationCode;
-      await sendVerificationEmail(email, verificationCode.code);
-
-      return res.send({
-        message: "Mã xác nhận đã được gửi đến địa chỉ email thành công",
-        payload: verificationCode,
-      });
-    } catch (error) {
-      console.error("Error during verification:", error);
-      return res
-        .status(500)
-        .json({ message: "Gửi mã xác nhận thất bại", error });
-    }
-  },
   register: async (req, res, next) => {
     try {
       const { firstName, lastName, email, phoneNumber, password, enteredCode } =
@@ -128,6 +61,123 @@ module.exports = {
         .json({ message: "Tạo tài khoản thất bại", error: err });
     }
   },
+
+  login: async (req, res, next) => {
+    try {
+      const { _id, firstName, lastName, email } = req.user;
+
+      const token = generateToken({
+        _id,
+        firstName,
+        lastName,
+        email,
+      });
+
+      const refreshToken = generateRefreshToken(_id);
+
+      return res.status(200).json({
+        message: "Login of user successfully",
+        token,
+        refreshToken,
+      });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: "Login of user failed", error: err });
+    }
+  },
+
+  refreshToken: async (req, res, next) => {
+    try {
+      const { refreshToken } = req.body;
+      JWT.verify(refreshToken, jwtSettings.SECRET, async (err, data) => {
+        if (err) {
+          return res.status(401).json({
+            message: "refreshToken is invalid",
+          });
+        } else {
+          const { id } = data;
+
+          const customer = await Customer.findOne({
+            _id: id,
+            isDeleted: false,
+          })
+            .select("-password")
+            .lean();
+
+          if (!customer) {
+            res.status(400).json({
+              statusCode: 400,
+              message: "Lỗi không tìm thấy người dùng",
+            });
+          }
+          const { _id, firstName, lastName, email } = customer;
+
+          const token = generateToken({
+            _id,
+            firstName,
+            lastName,
+            email,
+          });
+          return res.status(200).json({
+            message: "RefeshToken of user successfully",
+            token,
+          });
+        }
+      });
+    } catch (err) {
+      console.log("««««« err »»»»»", err);
+      res.status(400).json({
+        statusCode: 400,
+        message: "Lỗi",
+      });
+    }
+  },
+  
+  sendCode: async (req, res, next) => {
+    try {
+      const { email, phoneNumber, forgotPassword } = req.body;
+
+      const getEmailExits = Customer.findOne({ email });
+      const getPhoneExits = Customer.findOne({ phoneNumber });
+
+      const [foundEmail, foundPhoneNumber] = await Promise.all([
+        getEmailExits,
+        getPhoneExits,
+      ]);
+
+      const errors = [];
+      if (!forgotPassword) {
+        if (foundEmail) errors.push("Email đã tồn tại");
+        if (foundPhoneNumber) errors.push("Số điện thoại đã tồn tại");
+      } else {
+        if (!foundEmail) errors.push("Email tài khoản không tồn tại");
+      }
+
+      if (errors.length > 0) {
+        return res.status(404).json({
+          message: "Gửi mã xác nhận thất bại",
+          error: errors.join(", "),
+        });
+      }
+
+      // Tạo và gửi mã xác nhận
+      const verificationCode = generateVerificationCode();
+      storedVerificationCode = verificationCode;
+      await sendVerificationEmail(email, verificationCode.code);
+
+      return res.send({
+        message: "Mã xác nhận đã được gửi đến địa chỉ email thành công",
+        payload: verificationCode,
+      });
+    } catch (error) {
+      console.error("Error during verification:", error);
+      return res
+        .status(500)
+        .json({ message: "Gửi mã xác nhận thất bại", error });
+    }
+  },
+
   forgotPassword: async (req, res, next) => {
     try {
       const { email, newPassword, confirmPassword, enteredCode } = req.body;
@@ -178,52 +228,7 @@ module.exports = {
       });
     }
   },
-  refreshToken: async (req, res, next) => {
-    try {
-      const { refreshToken } = req.body;
-      JWT.verify(refreshToken, jwtSettings.SECRET, async (err, data) => {
-        if (err) {
-          return res.status(401).json({
-            message: "refreshToken is invalid",
-          });
-        } else {
-          const { id } = data;
 
-          const customer = await Customer.findOne({
-            _id: id,
-            isDeleted: false,
-          })
-            .select("-password")
-            .lean();
-
-          if (!customer) {
-            res.status(400).json({
-              statusCode: 400,
-              message: "Lỗi không tìm thấy người dùng",
-            });
-          }
-          const { _id, firstName, lastName, email } = customer;
-
-          const token = generateToken({
-            _id,
-            firstName,
-            lastName,
-            email,
-          });
-          return res.status(200).json({
-            message: "RefeshToken of user successfully",
-            token,
-          });
-        }
-      });
-    } catch (err) {
-      console.log("««««« err »»»»»", err);
-      res.status(400).json({
-        statusCode: 400,
-        message: "Lỗi",
-      });
-    }
-  },
   getMe: async (req, res, next) => {
     try {
       return res.status(200).json({
